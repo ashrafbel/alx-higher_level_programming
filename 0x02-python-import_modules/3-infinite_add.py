39e6bbf948dc3190d283650820f43a0e4547568c
ashrafbel <ashrafmus2@gmail.com>
2024-01-04 18:06:22 -0800

    3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,9 +3,9 @@
 if __name__ == "__main__":
     """Print the addition of all arguments."""
     import sys
-    
+
     S = 0
     for X in range(len(sys.argv) - 1):
         S += int(sys.argv[X + 1])
-        print(S)
-
+    
+    print(S)
