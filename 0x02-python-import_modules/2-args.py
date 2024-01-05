8629d082026f407f89e655d7d2b91ff6e9f7ca5c
ashrafbel <ashrafmus2@gmail.com>
2024-01-04 17:38:24 -0800

    2-args.py

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+    C = len(sys.argv)- 1
+    if C != 1:
+        print("{} arguments:".format(C))
+        for X in range(C):
+            print("{}: {}".format(X + 1,sys.argv[X + 1]))
+    else:
+        print("1 argument:")
+elif C == 0:
+    print("0 arguments.")
