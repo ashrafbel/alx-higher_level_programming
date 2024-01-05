e40101e6c64c4c2bb1c48eb2c68734db82163dfd
ashrafbel <ashrafmus2@gmail.com>
2024-01-04 18:41:33 -0800

    1-calculation.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
     import sys
-
     C = len(sys.argv) - 1
     if C == 0:
         print("0 arguments.")
@@ -12,4 +10,3 @@ if __name__ == "__main__":
         print("{} arguments:".format(C))
     for X in range(C):
         print("{}: {}".format(X + 1, sys.argv[X + 1]))
-
