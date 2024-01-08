374de697a6474e7ca7f53b045f6e3bf4bcc99581
ashrafbel <ashrafmus2@gmail.com>
2024-01-08 04:56:22 -0800

    5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
     NSTR = ""
-    for X in my_list:
-        if X not in ("c", "C")
-        NSTR += X
+    for X in my_string:
+        if X not in ("c", "C"):
+            NSTR += X
     return NSTR
