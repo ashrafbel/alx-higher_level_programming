9866d71533aa57101236d3c53b1c2de591b98da0
ashrafbel <ashrafmus2@gmail.com>
2024-01-10 18:28:44 -0800

    2-uniq_add.py

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,12 @@
+def roman_to_int(roman_string):
+    if not isinstance(roman_string, str) or roman_string is None:
+        return 0
+
+    roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
+                  'C': 100, 'D': 500, 'M': 1000}
+    rslt = 0
+    preval = 0
+    for X in reversed(roman_string):
+        preval = roman_dict[X]
+        rslt += preval if rslt < preval * 5 else -preval
+    return rslt
