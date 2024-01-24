cfbc5f3a83856212551b2377756088a730a76e7d
ashrafbel <ashrafmus2@gmial.com>
2024-01-23 18:02:52 -0800

    2-square.py

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Class Square
+"""
+
+
+class Square:
+    def __init__(self, size=0):
+        """Initialize new square
+        argument:
+            size (int): size of square
+        Raises:
+            typeeerror if size not integer
+            valueerror if size less than 0
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
