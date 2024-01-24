e3ef0e1e98858adb3cce87f514fead8003352765
ashrafbel <ashrafmus2@gmial.com>
2024-01-23 17:31:29 -0800

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Define square."""
+
+
+class Square:
+    """Rpresents square."""
+    def __init__(self, size):
+        """Initialize new instance of the square
+
+        Arguments:
+                 size int: the size of new square
+        """
+        self.__size = size
