02facdc605976831ae89b5facaf72fd619944c51
ashrafbel <ashrafmus2@gmial.com>
2024-01-24 18:56:59 -0800

    102-magic_calculation.py

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,11 +1,11 @@
 def magic_calculation(a, b):
     R = 0
-    for i in range(1, 3):
+    for s in range(1, 3):
         try:
             if i > a:
                 raise Exception('Too far')
             R += (a ** b) / i
-        except:
+        except Exception:
             R += a + b
             break
     return R
