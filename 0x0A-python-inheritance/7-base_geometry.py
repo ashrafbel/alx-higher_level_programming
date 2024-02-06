f18ab101082e3efd274f9b1c3eec6859a0ad0f03
ashrafbel <ashrafmus2@gmial.com>
2024-02-05 18:37:02 -0800

    7-base_geometry.py

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""""Module featuring Basegeometry"""
+
+
+class BaseGeometry:
+    """Class a baswgeometry"""
+    def area(self):
+        """Determinates the area"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Method for cheking th value"""
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/7-base_geometry.txt")
