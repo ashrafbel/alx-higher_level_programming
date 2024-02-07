77e40a8c28fec02f25b8d179601a38b3f5ac30e8
ashrafbel <ashrafmus2@gmial.com>
2024-02-06 16:50:54 -0800

    100-append_after.py

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def append_after(filename="", search_string="", new_string=""):
     "Inserts text after each line with a specific string in a file."
-    with open(filename, 'r') as Fl:
-        li_nes_txt = Fl.readlines()
-
-    with open(filename, 'w') as Fl:
-        for Ln in li_nes_txt:
-            Fl.write(Ln)
-            if search_string in Ln:
-                Fl.write(new_string)
+    text = ""
+    with open(filename) as r:
+        for line in r:
+            text += line
+            if search_string in line:
+                text += new_string
+    with open(filename, "w") as w:
+        w.write(text)
