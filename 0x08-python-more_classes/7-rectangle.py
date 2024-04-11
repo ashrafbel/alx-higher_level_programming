14551b7b69b5d5e74d8eaf2bae139f23d0e5def5
ashrafbel <ashrafmus2@gmail.com>
2024-04-10 17:53:09 -0700

    7-rectangle.py

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -1,20 +1,22 @@
 #!/usr/bin/python3
-"""Rectangle Cls"""
+"""Defines regtangele class"""
 
 
 class Rectangle:
     """Represent a rectangle."""
+    number_of_instances = 0
+    print_symbol = '#'
 
     def __init__(self, width=0, height=0):
-        "intialz cls"
+        "Create rectangle with the option to specify its width and height"
 
         self.width = width
         self.height = height
-        print_symbol = '#'
+        Rectangle.number_of_instances += 1
 
     @property
     def width(self):
-        """getter width react"""
+        """Retrieve the width of the rectangle."""
         return self.__width
 
     @width.setter
@@ -27,7 +29,7 @@ class Rectangle:
 
     @property
     def height(self):
-        "getter height rect"
+        "Assign a value to the width of the rectangle."
         return self.__height
 
     @height.setter
@@ -39,19 +41,21 @@ class Rectangle:
         self.__height = value
 
     def area(self):
-        """return the area Rect"""
+        """Return the area of the Rectangle."""
         return (self.__width * self.__height)
 
     def perimeter(self):
-        "perimeter of the rect"
+        "Retrieve the perimeter of the rectangle."
         if self.__width == 0 or self.__height == 0:
             return (0)
         return ((self.__width * 2) + (self.__height * 2))
 
     def __str__(self):
-        """string rect"""
+        """Return a string rectangle repreentation"""
         if self.__width == 0 or self.__height == 0:
             return ("")
+        return ((str(self.print_symbol) * self.width + "\n") *
+                self.height)[:-1]
 
         S_tr = []
         for X in range(self.__height):
@@ -60,6 +64,11 @@ class Rectangle:
                 S_tr.append("\n")
         return ("".join(S_tr))
 
+    def __repr__(self):
+        """Generate a string representation for the rectangle"""
+        return f"Rectangle({self.width}, {self.height})"
+
     def __del__(self):
-        "when instance rec detet printed"
+        """print a message when a Rectangle instance is deleted"""
         print("Bye rectangle...")
+        type(self).number_of_instances -= 1
