6166fdf31e8a3bb2cc9eb4c6b8a14347d121ff86
ashrafbel <ashrafmus2@gmail.com>
2024-04-10 18:01:23 -0700

    7-rectangle.py

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-"""Defines regtangele class"""
+"""Rectangle Cls"""
 
 
 class Rectangle:
     """Represent a rectangle."""
     number_of_instances = 0
     print_symbol = '#'
+    
 
     def __init__(self, width=0, height=0):
-        "Create rectangle with the option to specify its width and height"
+        "intialz cls"
 
         self.width = width
         self.height = height
@@ -16,7 +17,7 @@ class Rectangle:
 
     @property
     def width(self):
-        """Retrieve the width of the rectangle."""
+        """getter width react"""
         return self.__width
 
     @width.setter
@@ -29,7 +30,7 @@ class Rectangle:
 
     @property
     def height(self):
-        "Assign a value to the width of the rectangle."
+        "getter height rect"
         return self.__height
 
     @height.setter
@@ -41,35 +42,25 @@ class Rectangle:
         self.__height = value
 
     def area(self):
-        """Return the area of the Rectangle."""
+        """return the area Rect"""
         return (self.__width * self.__height)
 
     def perimeter(self):
-        "Retrieve the perimeter of the rectangle."
+        "perimeter of the rect"
         if self.__width == 0 or self.__height == 0:
             return (0)
         return ((self.__width * 2) + (self.__height * 2))
 
     def __str__(self):
-        """Return a string rectangle repreentation"""
+        """string rect"""
         if self.__width == 0 or self.__height == 0:
             return ("")
         return ((str(self.print_symbol) * self.width + "\n") *
                 self.height)[:-1]
+  
 
-        S_tr = []
-        for X in range(self.__height):
-            [S_tr.append('#') for Y in range(self.__width)]
-            if X != self.__height - 1:
-                S_tr.append("\n")
-        return ("".join(S_tr))
-
-    def __repr__(self):
-        """Generate a string representation for the rectangle"""
-        return f"Rectangle({self.width}, {self.height})"
 
     def __del__(self):
-        """print a message when a Rectangle instance is deleted"""
+        "when instance rec detet printed"
         print("Bye rectangle...")
         type(self).number_of_instances -= 1
-
