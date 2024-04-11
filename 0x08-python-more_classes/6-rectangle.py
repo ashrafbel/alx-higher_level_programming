29a956485e1f5a8592695a74c3cd35966d0ca3af
ashrafbel <ashrafmus2@gmail.com>
2024-04-10 17:35:03 -0700

    6-rectangle.py

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -4,12 +4,14 @@
 
 class Rectangle:
     """Represent a rectangle."""
+    number_of_instances = 0
 
     def __init__(self, width=0, height=0):
         "intialz cls"
 
         self.width = width
         self.height = height
+        Rectangle.number_of_instances += 1
 
     @property
     def width(self):
