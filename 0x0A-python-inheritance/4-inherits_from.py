f0010b950c92b41794a57b3eeaa2952fdb32700b
ashrafbel <ashrafmus2@gmail.com>
2024-04-15 16:02:07 -0700

    4-inherits_from.py

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"Module featuring the inherits_from function."
+"""Module featuring the inherits_from function."""
 
 
 def inherits_from(obj, a_class):
-    """check if an object is a true subclass of a class"""
+    """check if an object is a true subclass of a class."""
     b = isinstance(obj, a_class) and type(obj) != a_class
     return b
