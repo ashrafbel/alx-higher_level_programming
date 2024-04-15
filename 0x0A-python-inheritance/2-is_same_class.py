dfc50bf364417d9570c1db11c8a82631c53d1d8f
ashrafbel <ashrafmus2@gmail.com>
2024-04-15 15:20:44 -0700

    2-is_same_class.py

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"Module the is_same_class method"
+
+
+def is_same_class(obj, a_class):
+    "Verify whether an object belongs to a specified class"
+    return type(obj) == a_class
