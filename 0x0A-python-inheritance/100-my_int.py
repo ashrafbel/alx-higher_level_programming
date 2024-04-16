078760fed6db5b41cbcaa3889a019621187b5f40
ashrafbel <ashrafmus2@gmail.com>
2024-04-15 17:57:48 -0700

    100-my_int.py

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
+"""Defines a class MyInt that inherits from int."""
+
+
 class MyInt(int):
-    """A class named MyInt that inherits"""
+    """A rebel integer class with inverted equality operators."""
 
     def __eq__(self, other):
+        """Override the == operator to invert its behavior."""
         return super().__ne__(other)
 
     def __ne__(self, other):
+        """Override the != operator to invert its behavior."""
         return super().__eq__(other)
