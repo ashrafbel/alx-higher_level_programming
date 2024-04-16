f6b83c296274cdf41a3b8ced5bbd6bb6a339cfac
ashrafbel <ashrafmus2@gmail.com>
2024-04-15 17:48:58 -0700

    11-square.py

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Module rectangle class."""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """subclass representing a rectangle."""
+    def __init__(self, size):
+        """iniy react"""
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """Method for area of square"""
+        return self.__size ** 2
+
+    def __str__(self):
+        """ Returns a str representation square"""
+        return "[Square] " + str(self.__size) + "/" + str(self.__size)
+
