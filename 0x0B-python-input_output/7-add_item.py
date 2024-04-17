9dedfd4db0dcad342656ef7f3b4afbd072ca93e0
ashrafbel <ashrafmus2@gmail.com>
2024-04-16 18:56:58 -0700

    7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
-"Include all provided arguments in a Python list and store them in a file."
+"""Include all provided arguments in a Python list and store them in a file."""
 import sys
 
 
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+from save_to_json_file import save_to_json_file
+from load_from_json_file import load_from_json_file
 
 lisAr = list(sys.argv[1:])
 try:
     List_ = load_from_json_file("add_item.json")
-except Exception:
+except FileNotFoundError:
     List_ = []
-    
+
 List_.extend(lisAr)
 save_to_json_file(List_, "add_item.json")
