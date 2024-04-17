0a01875a2240a4fdb74ef0bf35c90f09b13a94d7
ashrafbel <ashrafmus2@gmail.com>
2024-04-16 17:43:15 -0700

    0-read_file.py

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Def function REad_file"""
+
+
+def read_file(filename=""):
+    """function read txt file"""
+    U = utf-8
+    with open(filename, encoding=U) as F_l:
+        print(f_l.read(), end="")
