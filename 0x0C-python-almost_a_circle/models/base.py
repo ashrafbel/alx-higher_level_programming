45248e2b0e3620c33955dd25294c7df6ec597c0f
ashrafbel <ashrafmus2@gmail.com>
2024-04-22 15:27:13 -0700

    base.py

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"Module defining the base class"
+from json import dumps, loads
+
+
+class Base:
+    "Base class responsible for handling unique identifiers."
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        "Initializes an instance"
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            Base.id = Base.__nb_objects
