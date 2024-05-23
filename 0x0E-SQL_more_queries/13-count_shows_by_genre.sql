474944c440fca4cbe65f885ae2b8fad8707f5fb3
ashrafbel <ashrafmus2@gmial.com>
2024-05-22 19:42:13 -0700

    13-count_shows_by_genre.sql

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -5,9 +5,3 @@ LEFT JOIN tv_show_genres ON genres.id = tv_show_genres.genre_id
 GROUP BY genre
 HAVING number_of_shows > 0
 ORDER BY number_of_shows DESC;
-SELECT genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
-FROM genres
-LEFT JOIN tv_show_genres ON genres.id = tv_show_genres.genre_id
-GROUP BY genre
-HAVING number_of_shows > 0
-ORDER BY number_of_shows DESC;
