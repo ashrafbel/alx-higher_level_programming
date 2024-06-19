1844839b4298377ac3b839d6d94daab0113136fc
ashrafbel <ashrafmus2@gmail.com>
2024-06-18 19:34:40 -0700

    6-multi_languages_loop.js

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// this script for prints 3 lines
+// by using array with loop
+
+const arrayofstring = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+
+for(let idx = 0; idx < 3; idx++) {
+	console.log(arrayofstring[idx]);
+}
