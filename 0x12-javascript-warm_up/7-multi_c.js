507fdc39f768bcc01af1734ca5684da8e04cab68
ashrafbel <ashrafmus2@gmail.com>
2024-06-18 19:47:42 -0700

    7-multi_c.js

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,12 +1,10 @@
 #!/usr/bin/node
-// this scritpt for prints x times “C is fun”
+
 
 if (isNaN(process.argv[2])) {
   console.log('Missing number of occurrences');
-
-
 } else {
-  for (let j = 0; j < parseInt(process.argv[2]); j++) {
+  for (let i = 0; i < parseInt(process.argv[2]); i++) {
     console.log('C is fun');
   }
 }
