040176f33f92ad04043523abc645012e68ae5b0d
ashrafbel <ashrafmus2@gmail.com>
2024-06-18 19:01:30 -0700

    3-value_argument.js

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -2,7 +2,8 @@
 // this script for prints the first argument passed to it
 
 if (process.argv[2] === undefined) {
-    console.log('No argument');
-  } else {
-    console.log(process.argv[2]);
-  }
+  console.log('No argument');
+
+} else {
+  console.log(process.argv[2]);
+}
