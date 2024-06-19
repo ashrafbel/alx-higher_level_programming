357b0b1d86da1d91d6100fae100fb0c138e51340
ashrafbel <ashrafmus2@gmail.com>
2024-06-18 18:21:08 -0700

    1-multi_languages.js

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,6 +1,8 @@
 #!/usr/bin/node
 // this script for print 3 lines
 
-console.log("C is fun");
-console.log("Python is cool");
-console.log("JavaScript is amazing");
+console.log('C is fun');
+
+console.log('Python is cool');
+
+console.log('JavaScript is amazing');
