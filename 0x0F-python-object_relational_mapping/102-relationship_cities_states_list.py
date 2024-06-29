1091d564c53e2e94efbb1aae36c910dd94363dd9
ashrafbel <ashrafmus2@gmail.com>
2024-06-28 19:34:36 -0700

    12-model_state_update_id_2.py

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -11,6 +11,7 @@ if __name__ == "__main__":
     from relationship_city import City
 
 
+
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
                            .format(sys.argv[1], sys.argv[2],
                                    sys.argv[3]), pool_pre_ping=True)
