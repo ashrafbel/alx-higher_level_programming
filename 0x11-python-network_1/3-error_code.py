364a2b05fe06b3daed21b76c22a6dd8897d869ab
ashrafbel <ashrafmus2@gmail.com>
2024-07-05 19:11:00 -0700

    3-error_code.py

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"Script that takes a URL, sends a request to it, and displays the response body."
+import urllib.request
+import urllib.error
+import sys
+
+
+if __name__ == "__main__":
+    try:
+        url = sys.argv[1]
+        r = urllib.request.Request(url)
+        with urllib.request.urlopen(r) as rs:
+            body = rs.read().decode('utf-8')
+            print(body)
+    except urllib.error.HTTPError as e:
+        print(f"Error code: {e.code}")
+
