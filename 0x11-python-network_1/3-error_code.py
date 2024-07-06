0af9198de8a1203ac7a8749c9b1eec6b4148e99d
ashrafbel <ashrafmus2@gmail.com>
2024-07-05 19:17:34 -0700

    3-error_code.py

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -13,5 +13,4 @@ if __name__ == "__main__":
             body = rs.read().decode('utf-8')
             print(body)
     except urllib.error.HTTPError as e:
-         print('Error code:', er.code)
-
+        print("Error code: {}".format(error.code))
