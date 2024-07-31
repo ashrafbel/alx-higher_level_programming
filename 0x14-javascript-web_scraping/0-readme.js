f28631571be31d2fa2192301d1928c71e7ea4483
ashrafbel <ashrafmus2@gmail.com>
2024-07-30 17:20:04 -0700

    0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,14 +1,12 @@
 #!/usr/bin/node
 
-
 const fs = require('fs');
-
 const fl_path = process.argv[2];
-fs.readFile(fl_path, 'utf8', (e, d) => {
-  if (e) {
-    console.error(e);
-    return;
-  }
-  console.log(d);
-});
 
+fs.readFile(fl_path, 'utf8', function (e, d) {
+  if (e) {
+    console.log(e);
+  } else {
+    process.stdout.write(d);
+  }
+});
