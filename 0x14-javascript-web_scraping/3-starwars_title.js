4d9da1186c2756e92ab80624544e8d6a72cdee44
ashrafbel <ashrafmus2@gmail.com>
2024-07-30 19:27:50 -0700

    3-starwars_title.js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
 const rt = require('request');
-const u = 'https://swapi-api.hbtn.io/api/films/'.concat(process.argv[2]);
+const urlFilms = 'https://swapi-api.hbtn.io/api/films/'.concat(process.argv[2]);
 
-rt(u, function (err, resp, body) {
+request(urlFilms, function (err, resp, body) {
   body = JSON.parse(body);
   console.log(body.title);
 });
